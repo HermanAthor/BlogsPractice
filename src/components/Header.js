d1be1e3d3e90cddbd1b580f2a228b29e96ecfe65
HermanAthor <106695994+HermanAthor@users.noreply.github.com>
2023-03-17 16:58:10 +0100

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import "./header.css"
+import {useState, useEffect} from "react"
+
 
 function Header() {
+  const[onScroll, setOnScroll] = useState(false)
+
+  function handleScroll(){
+    const scrollConst = window.pageYOffset
+    if(scrollConst > 0 && onScroll){
+      setOnScroll((prevOnScroll)=> !prevOnScroll)
+    }else if(scrollConst === 0 && !onScroll){
+      setOnScroll(true)
+    }
+  }
+
+  function handleS(){
+    setOnScroll((prevOnScroll)=> !prevOnScroll)
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll', handleS())
+    return ()=>{
+      window.removeEventListener('scroll', handleScroll())
+    }
+  },)
+  
+  
+
   return (
-    <div className='header-container'>
+     
+    <div className={onScroll? "header-container" : "hidden"}>
         <h1>INSTRUMENT</h1>
         <ul>
             <li>WHAT WE DO</li>
