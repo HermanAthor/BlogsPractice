772f3dd7d3fddc1c1ac9654eb9228398dacd9624
HermanAthor <106695994+HermanAthor@users.noreply.github.com>
2023-03-24 14:59:07 +0100

    added search functionality though the data needs to be modified

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,48 +3,65 @@ import "./info.css"
 
 
 function Info({data}) {
+    
     const[like, setlike]= React.useState(0)
     const[dislike, setDislike]= React.useState(0)
+    const[search, setSearch]= React.useState('')
     function handleLike(){
         return setlike(like + 1)
     }
     function handleDislike(){
         return (setDislike(like + 1))
-    }
-
-    
-    return (
-
-    <div>
-        {data.map((item) =>(
-            <div  className='info-container' key={item.id}>
-                 <img src={item.image} alt="imag"/>
-                    <h3>{item.name}</h3>
-                    <p>{item.title}</p>
-                    <button 
-                    className='like' 
-                    onClick={handleLike}
-                    key={item.id}
-                    value={item.id}
-                    >
-                        Like <span>{like}</span>
-                    </button>
-                    <button 
-                    className='dislike' 
-                    onClick={handleDislike}
-                    key={item.id}
-                    value={item.id}
-                    >
-                    Dislike <span>{dislike}</span>
-                    </button>
-                    <button>Follow +</button>
-        
-            </div>))
-        }
+    };
+    return(
+        <div>
+            <h1>Home</h1>
+            <input
+                type='text'
+                placeholder='Search Posts'
+                onChange={(e)=> setSearch(e.target.value)}
+            />
+            {
+                data.filter((post) => {
+                    if(search === ''){
+                        return post
+                    }else if (
+                        post.name.toLowerCase().includes(search.toLowerCase())||
+                        post.title.toLowerCase().includes(search.toLowerCase())
+                        ){
+                        return post
+                    }
+                }).map((item)=>{
+                    const {id, name, title,image} = item
+                    return(
+                        <div className='info-container' key={id}>
+                            <img src={image} alt="imag"/>
+                            <h3>{name}</h3>
+                            <p>{title}</p>
+                            <button 
+                            className='like' 
+                            onClick={handleLike}
+                            key={id}
+                            value={id}
+                            >
+                                Like <span>{like}</span>
+                            </button>
+                            <button 
+                            className='dislike' 
+                            onClick={handleDislike}
+                            key={id}
+                            value={id}
+                            >
+                            Dislike <span>{dislike}</span>
+                            </button>
+                            <button>Follow +</button>
+                        </div>
+                    )
+                })
+            }
             
-        
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Info
