5350a8aa51c0de7f2996ea312d3aba7f8b93739f
Alex <alexkogo63@gmail.com>
2023-03-29 14:13:57 +0300

    Contact Us changes

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,7 +6,6 @@ import WhatWeDo from './linked components/WhatWeDo'
 import WhoWeAre from './linked components/WhoWeAre'
 import Community from './linked components/Community'
 import Settings from './linked components/Settings'
-import Friends from './Right/Friends'
 import ContactCard from './Right/ContactCard'
 import Apps from './linked components/Apps'
 import TermsAndConditions from './linked components/TermsAndConditions'
@@ -27,7 +26,6 @@ function MainContent({search}) {
             <Route path='who-we-are' element={<WhoWeAre/>} />
             <Route path='community' element={<Community/>} />
             <Route path='settings' element={<Settings/>} />
-            <Route path='friends' element={<Friends/>} />
             <Route path='profile' element={<ContactCard/>} />
             <Route path='apps' element={<Apps/>} />
             
